04e39c19932c6aeebbbf3da2f923bd1dddfd20e9
romainhild <romain.hild@me.com>
2021-09-18 20:03:06 +0200

    add possibility to add an alarm

--- public/script.js
+++ public/script.js
@@ -1,18 +1,23 @@
-function makeHeader(id, time, state) {
+'use strict;'
+
+const daysFR = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+///////////// make html for an alarm
+function makeHeader(alarm) {
     let h1 = document.createElement("h1");
     h1.className = "accordion-header";
-    h1.setAttribute("id", `header${id}`);
+    h1.setAttribute("id", `header${alarm._id}`);
     let dflex = document.createElement("div");
     dflex.className = "d-flex";
     let button = document.createElement("button");
     button.className = "accordion-button collapsed";
     button.setAttribute("type", "button");
     button.setAttribute("data-bs-toggle", "collapse");
-    button.setAttribute("data-bs-target", `#collapse${id}`);
+    button.setAttribute("data-bs-target", `#collapse${alarm._id}`);
     button.setAttribute("aria-expanded", "false");
-    button.setAttribute("aria-controls", `collapse${id}`);
-    button.setAttribute("id", `textHeader${id}`);
-    button.innerHTML = `${time}`;
+    button.setAttribute("aria-controls", `collapse${alarm._id}`);
+    button.setAttribute("id", `textHeader${alarm._id}`);
+    button.innerHTML = `${alarm.time}`;
     dflex.appendChild(button);
     let divSwitch = document.createElement("div");
     divSwitch.className = "form-check form-switch d-flex align-items-start position-fixed";
@@ -20,11 +25,11 @@ function makeHeader(id, time, state) {
     let switch1 = document.createElement("input");
     switch1.className = "form-check-input";
     switch1.setAttribute("type", "checkbox");
-    switch1.setAttribute("id", `switch${id}`);
-    if( state )
+    switch1.setAttribute("id", `switch${alarm._id}`);
+    if( alarm.state )
 	switch1.setAttribute('checked', '');
     switch1.onchange = function() {
-	stateChanged(id, switch1.checked);
+	stateChanged(alarm._id, switch1.checked);
     };
     divSwitch.appendChild(switch1);
     dflex.appendChild(divSwitch);
@@ -32,31 +37,31 @@ function makeHeader(id, time, state) {
     return h1;
 }
 
-function makeTime(alarm) {
+function makeTime(id, time) {
     let div = document.createElement('div');
     div.className = "row mb-3";
     let label = document.createElement('label');
-    label.setAttribute("for", `time${alarm._id}`);
+    label.setAttribute("for", `time${id}`);
     label.className = "col-sm-3 col-form-label";
     label.innerHTML = "Heure";
     div.appendChild(label);
     let divIn = document.createElement('div');
     divIn.className = "col-sm-9";
-    let time = document.createElement('input');
-    time.setAttribute("type", "time");
-    time.setAttribute("id", `time${alarm._id}`);
-    time.setAttribute("name", `time${alarm._id}`);
-    time.setAttribute("required", '');
-    time.setAttribute("value", alarm.time);
-    time.onchange = function() {
-	timeChanged(alarm._id, time.value, alarm.time);
+    let timeIn = document.createElement('input');
+    timeIn.setAttribute("type", "time");
+    timeIn.setAttribute("id", `time${id}`);
+    timeIn.setAttribute("name", `time${id}`);
+    timeIn.setAttribute("required", '');
+    timeIn.setAttribute("value", time);
+    timeIn.onchange = function() {
+	timeChanged(id, timeIn.value, time);
     };
-    divIn.appendChild(time);
+    divIn.appendChild(timeIn);
     div.appendChild(divIn);
     return div;
 }
 
-function makeRepetition(alarm) {
+function makeRepetition(id, repetition) {
     let div = document.createElement('div');
     div.className = "row mb-3";
     let label = document.createElement('label');
@@ -69,22 +74,22 @@ function makeRepetition(alarm) {
     divGr.className = "btn-group";
     divGr.setAttribute('role', 'group');
     divGr.setAttribute("aria-label","Basic example");
-    for (day in alarm.repetition) {
+    for (day in repetition) {
 	let input = document.createElement("input");
 	input.setAttribute("type", "checkbox");
 	input.setAttribute("class", "btn-check");
 	input.setAttribute("autocomplete", "off");
-	input.setAttribute("id", `check${day}${alarm._id}`);
+	input.setAttribute("id", `check${day}${id}`);
 	input.setAttribute("value", day);
-	if( alarm.repetition[day] )
+	if( repetition[day] )
 	    input.setAttribute("checked", "");
 	input.onchange = function() {
-	    repetChanged(alarm._id, input.value, input.checked);
+	    repetChanged(id, input.value, input.checked);
 	};
 	divGr.appendChild(input);
 	let label = document.createElement("label");
 	label.setAttribute("class", "btn btn-primary");
-	label.setAttribute("for", `check${day}${alarm._id}`);
+	label.setAttribute("for", `check${day}${id}`);
 	label.innerHTML = `${day.charAt(0)}`;
 	divGr.appendChild(label);
     }
@@ -93,57 +98,58 @@ function makeRepetition(alarm) {
     return div;
 }
 
-function makeVolume(alarm) {
+function makeVolume(id, volume) {
     let div = document.createElement('div');
     div.className = "row mb-3";
     let label = document.createElement('label');
-    label.setAttribute("for", `volume${alarm._id}`);
+    label.setAttribute("for", `volume${id}`);
     label.className = "col-sm-3 col-form-label";
-    label.setAttribute("id", `volumeLabel${alarm._id}`);
-    label.innerHTML = `Volume ${alarm.volume}%`;
+    label.setAttribute("id", `volumeLabel${id}`);
+    label.innerHTML = `Volume ${volume}%`;
     div.appendChild(label);
     let divIn = document.createElement('div');
     divIn.className = "col-sm-9";
-    let volume = document.createElement('input');
-    volume.setAttribute("type", "range");
-    volume.setAttribute("class", "form-range");
-    volume.setAttribute("min", "0");
-    volume.setAttribute("max", "100");
-    volume.setAttribute("step", "1");
-    volume.setAttribute("id", `volume${alarm._id}`);
-    volume.setAttribute("value", alarm.volume);
-    volume.onchange = function() {
-	volumeChanged(alarm._id, volume.value, alarm.volume);
+    let volumeIn = document.createElement('input');
+    volumeIn.setAttribute("type", "range");
+    volumeIn.setAttribute("class", "form-range");
+    volumeIn.setAttribute("min", "0");
+    volumeIn.setAttribute("max", "100");
+    volumeIn.setAttribute("step", "1");
+    volumeIn.setAttribute("id", `volume${id}`);
+    volumeIn.setAttribute("value", volume);
+    volumeIn.onchange = function() {
+	volumeChanged(id, volumeIn.value, volume);
     };
-    divIn.appendChild(volume);
+    divIn.appendChild(volumeIn);
     div.appendChild(divIn);
     return div;
 }
 
-function makePlaylist(alarm) {
+function makePlaylist(id, playlist) {
     let div = document.createElement('div');
     div.className = "row mb-3";
     let label = document.createElement('label');
-    label.setAttribute("for", `playlist${alarm._id}`);
+    label.setAttribute("for", `playlist${id}`);
     label.className = "col-sm-3 col-form-label";
     label.innerHTML = `Playlist`;
     div.appendChild(label);
     let divIn = document.createElement('div');
     divIn.className = "col-sm-9";
-    let playlist = document.createElement('select');
-    playlist.className = "form-select";
-    playlist.setAttribute("aria-label", "Select Playlist");
+    let playlistIn = document.createElement('select');
+    playlistIn.className = "form-select";
+    playlistIn.setAttribute("aria-label", "Select Playlist");
+    playlistIn.setAttribute("id", `playlist${id}`);
     let option = document.createElement("option");
-    option.setAttribute("value", alarm.playlist);
+    option.setAttribute("value", playlist);
     option.setAttribute("selected", "");
-    option.innerHTML = alarm.playlist;
-    playlist.appendChild(option);
-    divIn.appendChild(playlist);
+    option.innerHTML = playlist;
+    playlistIn.appendChild(option);
+    divIn.appendChild(playlistIn);
     div.appendChild(divIn);
     return div;
 }
 
-function makeDelete(alarm) {
+function makeDelete(id) {
     let div = document.createElement('div');
     div.className = "row mb-3 justify-content-center";
     let button = document.createElement('button');
@@ -151,7 +157,7 @@ function makeDelete(alarm) {
     button.className = "btn btn-danger col-sm-3";
     button.innerHTML = "Supprimer";
     button.onclick = function() {
-	alarmDelete(alarm._id);
+	alarmDelete(id);
     };
     div.appendChild(button);
     return div;
@@ -166,15 +172,15 @@ function makeCollapse(alarm) {
     let bodyDiv = document.createElement('div');
     bodyDiv.className = "accordion-body";
     let form = document.createElement('form');
-    let time = makeTime(alarm);
+    let time = makeTime(alarm._id, alarm.time);
     form.appendChild(time);
-    let repet = makeRepetition(alarm);
+    let repet = makeRepetition(alarm._id, alarm.repetition);
     form.appendChild(repet);
-    let volume = makeVolume(alarm);
+    let volume = makeVolume(alarm._id, alarm.volume);
     form.appendChild(volume);
-    let playlist = makePlaylist(alarm);
+    let playlist = makePlaylist(alarm._id, alarm.playlist);
     form.appendChild(playlist);
-    let del = makeDelete(alarm);
+    let del = makeDelete(alarm._id);
     form.appendChild(del);
     bodyDiv.appendChild(form);
     div.appendChild(bodyDiv);
@@ -186,13 +192,14 @@ function makeAlarm(alarm) {
     var alarmItem = document.createElement('div');
     alarmItem.className = "accordion-item";
     alarmItem.setAttribute("id", `item${alarm._id}`);
-    let header = makeHeader(alarm._id, alarm.time, alarm.state);
+    let header = makeHeader(alarm);
     let collapse = makeCollapse(alarm);
     alarmItem.appendChild(header);
     alarmItem.appendChild(collapse);
     accordion.appendChild(alarmItem);
 }
 
+//////////////// handle changes to the alarms
 function stateChanged(id, state) {
     axios.patch(`alarms/${id}`, {state: `${state}`})
 	.then(function(response) {
@@ -207,43 +214,56 @@ function stateChanged(id, state) {
 }
 
 function timeChanged(id, time) {
-    axios.patch(`alarms/${id}`, {time: `${time}`})
-	.then(function(response) {
-	    alarms.get(id).time = time;
-	    document.getElementById(`textHeader${id}`).innerHTML = time;
-	})
-	.catch(function(error) {
-	    console.log(error);
-	    document.getElementById(`time${id}`).value = alarms.get(id).time;
-	});
+    if( id ) {
+	axios.patch(`alarms/${id}`, {time: `${time}`})
+	    .then(function(response) {
+		alarms.get(id).time = time;
+		document.getElementById(`textHeader${id}`).innerHTML = time;
+	    })
+	    .catch(function(error) {
+		console.log(error);
+		document.getElementById(`time${id}`).value = alarms.get(id).time;
+	    });	
+    }
+    else {
+	let t = document.getElementById("time");
+	let p = document.getElementById("playlist");
+	if( t.value && p.value ) {
+	    document.getElementById("saveNewAlarmButton").disabled = false;
+	}
+	else {
+	    document.getElementById("saveNewAlarmButton").disabled = true;
+	}
+    }
 }
 
 function repetChanged(id, day, state) {
-    let newRepetition = Object.assign({}, alarms.get(id).repetition);
-    newRepetition[day] = state;
-    console.log(alarms.get(id).repetition);
-    console.log(newRepetition);
-    axios.patch(`alarms/${id}`, {repetition: newRepetition})
-	.then(function(response) {
-	    alarms.get(id).repetition = newRepetition;
-	})
-	.catch(function(error) {
-	    console.log(error);
-	    document.getElementById(`check${day}${id}`).checked = !state;
-	});
-    console.log(`alarm ${id} changed repetition for ${day} to ${state}`);
+    if( id ) {
+	let newRepetition = Object.assign({}, alarms.get(id).repetition);
+	newRepetition[day] = state;
+	axios.patch(`alarms/${id}`, {repetition: newRepetition})
+	    .then(function(response) {
+		alarms.get(id).repetition = newRepetition;
+	    })
+	    .catch(function(error) {
+		console.log(error);
+		document.getElementById(`check${day}${id}`).checked = !state;
+	    });
+    }
 }
 
 function volumeChanged(id, volume) {
-    axios.patch(`alarms/${id}`, {volume: volume})
-	.then(function(response) {
-	    alarms.get(id).volume = volume;
-	    document.getElementById(`volumeLabel${id}`).innerHTML = `Volume ${volume}%`;
-	})
-	.catch(function(error) {
-	    console.log(error);
-	    document.getElementById(`volume${id}`).value = alarms.get(id).volume;
-	});
+    if( id ) {
+	axios.patch(`alarms/${id}`, {volume: volume})
+	    .then(function(response) {
+		alarms.get(id).volume = volume;
+		document.getElementById(`volumeLabel${id}`).innerHTML = `Volume ${volume}%`;
+	    })
+	    .catch(function(error) {
+		console.log(error);
+		document.getElementById(`volume${id}`).value = alarms.get(id).volume;
+	    });
+    }
 };
 
 function alarmDelete(id) {
@@ -257,6 +277,50 @@ function alarmDelete(id) {
 	});
 }
 
+///////////////// add an alarm
+function addAlarm() {
+    console.log("add alarm");
+    let form = document.createElement('form');
+    let time = makeTime('', '');
+    form.appendChild(time);
+    let repet = makeRepetition('', {"Lundi":false,"Mardi":false,"Mercredi":false,"Jeudi":false,"Vendredi":false,"Samedi":false,"Dimanche":false});
+    form.appendChild(repet);
+    let volume = makeVolume('', 50);
+    form.appendChild(volume);
+    let playlist = makePlaylist('', 'test'); // to change !!!
+    form.appendChild(playlist);
+    document.getElementById('newAlarmBody').appendChild(form);
+    var myModal = new bootstrap.Modal(document.getElementById('newAlarmModal'));
+    myModal.show();
+}
+
+function saveAlarm() {
+    let time = document.getElementById("time").value;
+    let repetition = {};
+    for( let day of daysFR )
+	repetition[day] = document.getElementById(`check${day}`).checked;
+    let volume = document.getElementById("volume").value;
+    let playlist = document.getElementById("playlist").value;
+    axios.post('alarms',
+	       { time: time,
+		 repetition: repetition,
+		 volume: volume,
+		 playlist: playlist,
+		 state: true })
+	.then(function(response) {
+	    alarms.set(response.data._id, response.data);
+	    makeAlarm(response.data);
+	    var myModalEl = document.getElementById('newAlarmModal');
+	    var myModal = bootstrap.Modal.getInstance(myModalEl);
+	    myModal.hide();
+	})
+	.catch(function(error) {
+	    console.log(error);
+	});;
+}
+
+document.getElementById("addButton").onclick = addAlarm;
+document.getElementById("saveNewAlarmButton").onclick = saveAlarm;
 
 var alarms = new Map([]);
 axios.get("alarms")
@@ -266,6 +330,5 @@ axios.get("alarms")
 	alarms.forEach(makeAlarm);
     })
     .catch(function (error) {
-	// handle error
 	console.log(error);
     });
